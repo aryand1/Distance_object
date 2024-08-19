e7136396d9fcf0013525b6688c5d012e04cc3871
Aryan Singh Dalal <116189291+aryand1@users.noreply.github.com>
2024-08-19 12:20:26 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Add any JavaScript-based interactions here.
+    const mainImage = document.querySelector('.network-graph');
+    adjustImageSize(mainImage);
 });
+
+window.addEventListener('resize', function() {
+    const mainImage = document.querySelector('.network-graph');
+    adjustImageSize(mainImage);
+});
+
+function adjustImageSize(imageElement) {
+    if (window.innerWidth < 600) {
+        imageElement.style.width = '100%';
+    } else {
+        imageElement.style.width = '80%';
+    }
+}
